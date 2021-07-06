71fb539d0c2cda6035960afbe4e6d31cec6a2189
Rinn4747 <86368454+Rinn4747@users.noreply.github.com>
2021-07-06 18:26:02 +0200

    more refacto to make it easier to read
    from math.distance3d to iAmOnLocRange()

--- backtosleep.lua
+++ backtosleep.lua
@@ -1,6 +1,6 @@
 --using Kitanoi draw function / menu snippet from discord #lua-help
 --Rinn#4747
---v0.0.11
+--v0.0.12
 
 backtosleep= { }
 backtosleep.toggletable = {
@@ -61,15 +61,38 @@ backtosleep.pos.maps = {
 }
 
 backtosleep.pos.aetheryteID = {
-			["Gridania"] = 2,
-			["Limsa"] = 8,
-			["Ul'dah"] = 9,
-			["Ishgard"] = 70,
-			["Kugane"] = 111,
-			["Crystarium"] = 133,
+			["Gridania"] = {id = 2, tpradius = 4},
+			["Limsa"] = {id = 8, tpradius = 8},
+			["Ul'dah"] = {id = 9, tpradius = 4},
+			["Ishgard"] = {id = 70, tpradius = 8},
+			["Kugane"] = {id = 111, tpradius = 8},
+			["Crystarium"] = {id = 133, tpradius = 9},
 }
 
-local function iamonmap(string)
+backtosleep.pos.loc = {
+			["Limsa Aetheryte"] = {x = -84.03, y= 20.77, z= 0.02, radius = 20 },
+			["Limsa FC Chest Area"] = {x=-201.49,y=15.98,z=50.6, radius = 25},
+			["Limsa FC Chest Stop"] = {x=-199.58,y=16.00,z=57.51, radius = 8},
+			["Limsa MB Area"] = {x=-217.75,y=16.00,z=47.32, radius = 10},
+			["Limsa MB Stop"] = {x=-224.28,y=16.00,z=46.66, radius = 4},
+			["Ul'dah Aetheryte"] = {x=-144.41030883789, y = -3.1548881530762, z = -169.6149597168, radius = 10},
+			["Ul'dah Inn Area"] = {x=39.32,y=8.00,z=-98.18, radius= 40},
+			["Ishgard Aetheryte"] = {x=-63.98,y=11.15,z=43.99, radius = 15},
+			["Ishgard Inn Area"] = {x=66.40,y=23.98,z=18.08, radius = 40},
+			["Kugane Aetheryte"] = {x=47.5,y=8.44,z=-37.31, radius = 15},
+			["Kugane Inn Area"] = {x=-84.73,y=18.05,z=-181.11, radius = 25},
+			["Kugane Leves Area"] = {x=34.8,y=3,z=-60.91, radius = 40},
+			["Kugane Leves Stop"] = {x=21.03,y=0.00,z=-76.93, radius = 4},
+			["Kugane MB Area"] = {x=12.28,y=4.00,z=60.91, radius = 25},
+			["Kugane MB Stop"] = {x=2.47,y=4.00,z=51.23, radius = 4},
+			["Crystarium Aetheryte"] = {x=-65.02,y=4.53,z=0.02, radius = 15},
+			["Crystarium Inn Area"] = {x=52.74,y=1.71,z=232.94, radius = 25},
+			["Crystarium Leves Area"] = {x=-61.98,y=20.00,z=-138.94, radius = 45},
+			["Crystarium Leves Stop"] = {x=-72.35,y=20.00,z=-111.34, radius = 4},
+
+}
+
+local function iAmOnMap(string)
 	if Player.localmapid == backtosleep.pos.maps[string] then
 		return true
 	else
@@ -77,8 +100,19 @@ local function iamonmap(string)
 	end
 end
 
+local function iAmOnLocRange(string)
+	local lx = backtosleep.pos.loc[string].x
+	local ly = backtosleep.pos.loc[string].y
+	local lz = backtosleep.pos.loc[string].z
+	local lr = backtosleep.pos.loc[string].radius
 
-
+	if math.distance3d(Player.pos,{x= lx,y= ly ,z= lz}) <= lr then
+		return true
+	else
+		return false
+	end
+	
+end
 
 
 function backtosleep.ModuleInit()
@@ -204,7 +238,7 @@ end
 local function teleportTo(string)
 	if TimeSince(backtosleepteleportnow) > 6000 then
 		if not  MIsCasting() then
-			Player:Teleport(backtosleep.pos.aetheryteID[string])
+			Player:Teleport(backtosleep.pos.aetheryteID[string].id)
 		end
 		backtosleepteleportnow = Now()
 		backtosleepmovenow = Now()
@@ -334,36 +368,36 @@ end
 function backtosleep.OnUpdateHandler( Event, ticks )	
 --ROOST
 	if backtosleep.runningroost then
-		if iamonmap("The Roost")then
+		if iAmOnMap("The Roost")then
 			stopRunning()
-		elseif not iamonmap("Old Gridania") then
+		elseif not iAmOnMap("Old Gridania") then
 			teleportTo("Gridania")		
-		elseif iamonmap("Old Gridania") then
+		elseif iAmOnMap("Old Gridania") then
 			innGuyTravel(25.56,-8.00,97.94,1000102)
 		end
 	end
 --MIZZENMAST	
 	if backtosleep.runningmizzen then
-		if iamonmap("Mizzenmast") then
+		if iAmOnMap("Mizzenmast") then
 			stopRunning()
-		elseif not iamonmap("Limsa Lominsa Upper Deck") and not iamonmap("Limsa Lominsa Lower Deck") then
+		elseif not iAmOnMap("Limsa Lominsa Upper Deck") and not iAmOnMap("Limsa Lominsa Lower Deck") then
 			teleportTo("Limsa")
-		elseif iamonmap("Limsa Lominsa Lower Deck") then
+		elseif iAmOnMap("Limsa Lominsa Lower Deck") then
 			cityBigAetheryteTravel(-84.03,20.77,0.02,8,8,"The Aftcastle.")
-		elseif iamonmap("Limsa Lominsa Upper Deck") then
+		elseif iAmOnMap("Limsa Lominsa Upper Deck") then
 			innGuyTravel(12.83,40,11.66,1000974)
 		end		
 	end
 --HOURGLASS	
 	if backtosleep.runninghourglass then
-		if iamonmap("The Hourglass") then
+		if iAmOnMap("The Hourglass") then
 			stopRunning()
-		elseif not iamonmap("Steps of Nald") then
+		elseif not iAmOnMap("Steps of Nald") then
 			teleportTo("Ul'dah")
-		elseif iamonmap("Steps of Nald") then
-			if  math.distance3d(Player.pos,{x=-144.41030883789, y = -3.1548881530762, z = -169.6149597168}) <= 10 then
+		elseif iAmOnMap("Steps of Nald") then
+			if  iAmOnLocRange("Ul'dah Aetheryte") then
 				cityBigAetheryteTravel(-144.41030883789,-3.1548881530762,-169.6149597168,4,9,"Adventurers' Guild.")
-			elseif math.distance3d(Player.pos,{x=39.32,y=8.00,z=-98.18})<=40 then
+			elseif iAmOnLocRange("Ul'dah Inn Area") then
 				innGuyTravel(29.06,7.00,-80.31,1001976)				
 			else
 				teleportTo("Ul'dah")
@@ -372,14 +406,14 @@ function backtosleep.OnUpdateHandler( Event, ticks )
 	end
 --CloudNine
 	if backtosleep.runningcloudnine then
-		if iamonmap("Cloudnine") then
+		if iAmOnMap("Cloudnine") then
 			stopRunning()
-		elseif not iamonmap("Foundation") then
+		elseif not iAmOnMap("Foundation") then
 			teleportTo("Ishgard")
-		elseif iamonmap("Foundation") then
-			if math.distance3d(Player.pos,{x=-63.98,y=11.15,z=43.99})<=15 then
+		elseif iAmOnMap("Foundation") then
+			if iAmOnLocRange("Ishgard Aetheryte") then
 				cityBigAetheryteTravel(-63.98,11.15,43.99,8,70,"The Forgotten Knight.")
-			elseif math.distance3d(Player.pos,{x=66.40,y=23.98,z=18.08})<=40 then
+			elseif iAmOnLocRange("Ishgard Inn Area") then
 				innGuyTravel(84.83,15.09,33.61,1011193)												
 			else
 				teleportTo("Ishgard")
@@ -388,11 +422,11 @@ function backtosleep.OnUpdateHandler( Event, ticks )
 	end
 --BokairoInn
 	if backtosleep.runningbokairoinn then
-		if iamonmap("Bokairo Inn") then
+		if iAmOnMap("Bokairo Inn") then
 			stopRunning()
-		elseif not iamonmap("Kugane") then
+		elseif not iAmOnMap("Kugane") then
 			teleportTo("Kugane")
-		elseif iamonmap("Kugane") then
+		elseif iAmOnMap("Kugane") then
 			if math.distance3d(Player.pos,{x=47.5,y=8.44,z=-37.31})<=15 then
 				cityBigAetheryteTravel(47.5,8.44,-37.31,8,111,"Bokairo Inn.")
 			elseif math.distance3d(Player.pos,{x=-84.73,y=18.05,z=-181.11})<=25 then
@@ -404,14 +438,14 @@ function backtosleep.OnUpdateHandler( Event, ticks )
 	end
 --ThePendants	
 	if backtosleep.runningthependants then
-		if iamonmap("The Pendants") then
+		if iAmOnMap("The Pendants") then
 			stopRunning()
-		elseif not iamonmap("Crystarium") then
+		elseif not iAmOnMap("Crystarium") then
 			teleportTo("Crystarium")
-		elseif iamonmap("Crystarium") then
-			if math.distance3d(Player.pos,{x=-65.02,y=4.53,z=0.02})<=15 then
+		elseif iAmOnMap("Crystarium") then
+			if iAmOnLocRange("Crystarium Aetheryte") then
 				cityBigAetheryteTravel(-65.02,4.53,0.02,9,133,"The Pendants.")
-			elseif math.distance3d(Player.pos,{x=52.74,y=1.71,z=232.94})<=25 then
+			elseif iAmOnLocRange("Crystarium Inn Area") then
 				innGuyTravel(62.71,1.72,247.85,1027231)												
 			else
 				teleportTo("Crystarium")
@@ -420,18 +454,19 @@ function backtosleep.OnUpdateHandler( Event, ticks )
 	end
 --FC Chest Limsa
 	if backtosleep.runningfclimsa then
-		if iamonmap("Limsa Lominsa Lower Deck") and math.distance3d(Player.pos,{x=-199.58,y=16.00,z=57.51})<=8 then
+		if iAmOnMap("Limsa Lominsa Lower Deck") and iAmOnLocRange("Limsa FC Chest Stop") then
 			if not Player:IsMoving() then
 				stopRunning()
 				backtosleeprandompoint = nil
 				d("stop running")
 			end
-		elseif not iamonmap("Limsa Lominsa Lower Deck") then
+		elseif not iAmOnMap("Limsa Lominsa Lower Deck") then
 			teleportTo("Limsa")
-		elseif iamonmap("Limsa Lominsa Lower Deck") then
-			if math.distance3d(Player.pos,{x=-84.03,y=20.77,z=0.02})<=20 then
+		elseif iAmOnMap("Limsa Lominsa Lower Deck") then
+			--if math.distance3d(Player.pos,{x=-84.03,y=20.77,z=0.02})<=20 then
+			if iAmOnLocRange("Limsa Aetheryte") then
 				cityBigAetheryteTravel(-84.03,20.77,0.02,8,8,"Hawkers' Alley.")
-			elseif math.distance3d(Player.pos,{x=-201.49,y=15.98,z=50.6})<=25 then
+			elseif iAmOnLocRange("Limsa FC Chest Area") then
 				randomMoveTo(-199.58,16.00,57.51,0,6)				
 			else
 				teleportTo("Limsa")
@@ -440,7 +475,7 @@ function backtosleep.OnUpdateHandler( Event, ticks )
 	end
 --Central Aetheryte Limsa
 	if backtosleep.runningcentrallimsa then
-		if math.distance3d(Player.pos,{x=-84.03,y=20.77,z=0.02})<=20 then
+		if iAmOnLocRange("Limsa Aetheryte") then
 			cityBigAetheryteTeleport(-84.03,20.77,0.02,6)
 		else
 			teleportTo("Limsa")
@@ -450,22 +485,22 @@ function backtosleep.OnUpdateHandler( Event, ticks )
 --MB Limsa
 	if backtosleep.runningmblimsa then
 		--map and range where it ends
-		if iamonmap("Limsa Lominsa Lower Deck") and math.distance3d(Player.pos,{x=-224.28,y=16.00,z=46.66})<=4 then
+		if iAmOnMap("Limsa Lominsa Lower Deck") and iAmOnLocRange("Limsa MB Stop") then
 			if not Player:IsMoving() then
 				stopRunning()
 				backtosleeprandompoint = nil
 				d("stop running")
 			end
 		--if not on map tp to it
-		elseif not iamonmap("Limsa Lominsa Lower Deck") then
+		elseif not iAmOnMap("Limsa Lominsa Lower Deck") then
 			teleportTo("Limsa")
 		--if on map but not objective...
-		elseif iamonmap("Limsa Lominsa Lower Deck") then
+		elseif iAmOnMap("Limsa Lominsa Lower Deck") then
 			--if around aetheryte interact and aetheryte to hawkers' Alley
-			if math.distance3d(Player.pos,{x=-84.03,y=20.77,z=0.02})<=20 then
+			if iAmOnLocRange("Limsa Aetheryte") then
 				cityBigAetheryteTravel(-84.03,20.77,0.02,8,8,"Hawkers' Alley.")
 			--if somewhere midway between aetheryte Hawkers' Alley and objective 		
-			elseif math.distance3d(Player.pos,{x=-217.75,y=16.00,z=47.32})<=10 then
+			elseif iAmOnLocRange("Limsa MB Area") then
 				--random move to max radius < range where it ends (same pos as where it ends)
 				randomMoveTo(-224.28,16.00,46.66,0,3)				
 			else
@@ -476,19 +511,19 @@ function backtosleep.OnUpdateHandler( Event, ticks )
 --Leve Kugane
 	if backtosleep.runninglevekugane then
 		--map and range where it ends
-		if iamonmap("Kugane") and math.distance3d(Player.pos,{x=21.03,y=0.00,z=-76.93})<=4 then
+		if iAmOnMap("Kugane") and iAmOnLocRange("Kugane Leves Stop") then
 			if not Player:IsMoving() then
 				stopRunning()
 				backtosleeprandompoint = nil
 				d("stop running")
 			end
 		--if not on map tp to it
-		elseif not iamonmap("Kugane") then
+		elseif not iAmOnMap("Kugane") then
 			teleportTo("Kugane")
 		--if on map but not objective...
-		elseif iamonmap("Kugane") then
+		elseif iAmOnMap("Kugane") then
 			--if somewhere midway between aetheryte and objective 		
-			if math.distance3d(Player.pos,{x=34.8,y=3,z=-60.91})<=40 then
+			if iAmOnLocRange("Kugane Leves Area") then
 				--random move to max radius < range where it ends (same pos as where it ends)
 				randomMoveTo(21.03,0.00,-76.93,0,3)				
 			else
@@ -499,28 +534,28 @@ function backtosleep.OnUpdateHandler( Event, ticks )
 --MB Kugane
 	if backtosleep.runningmbkugane then
 		--map and range where it ends
-		if iamonmap("Kugane") and math.distance3d(Player.pos,{x=2.47,y=4.00,z=51.23})<=4 then
+		if iAmOnMap("Kugane") and iAmOnLocRange("Kugane MB Stop") then
 			if not Player:IsMoving() then
 				stopRunning()
 				backtosleeprandompoint = nil
 				d("stop running")
 			end
 		--if not on map tp to it
-		elseif not iamonmap("Kugane") then
+		elseif not iAmOnMap("Kugane") then
 			teleportTo("Kugane")
 		--if on map but not objective...
-		elseif iamonmap("Kugane") then
+		elseif iAmOnMap("Kugane") then
 			--if around aetheryte interact and aetheryte to hawkers' Alley
-			if math.distance3d(Player.pos,{x=47.5,y=8.44,z=-37.31})<=15 then
+			if iAmOnLocRange("Kugane Aetheryte") then
 				cityBigAetheryteTravel(47.5,8.44,-37.31,8,111,"Kogane Dori Markets.")
 			--if somewhere midway between aetheryte and objective 		
-			elseif math.distance3d(Player.pos,{x=12.28,y=4.00,z=60.91})<=25 then
+			elseif iAmOnLocRange("Kugane MB Area") then
 				--random move to max radius < range where it ends (same pos as where it ends)
 				if TimeSince(backtosleepmovenow) > 10000 then
 					randomMoveTo(2.47,4.00,51.23,0,3)				
 				end
 			else
-				if math.distance3d(Player.pos,{x=34.8,y=3,z=-60.91})<=40 then
+				if iAmOnLocRange("Kugane Leves Area") then
 					cityBigAetheryteTravel(47.5,8.44,-37.31,8,111,"Kogane Dori Markets.")
 					backtosleepmovenow = Now()
 				else
@@ -532,22 +567,22 @@ function backtosleep.OnUpdateHandler( Event, ticks )
 --Leve Crystarium
 	if backtosleep.runninglevecrystarium then
 		--map and range where it ends
-		if iamonmap("Crystarium") and math.distance3d(Player.pos,{x=-72.35,y=20.00,z=-111.34})<=4 then
+		if iAmOnMap("Crystarium") and iAmOnLocRange("Crystarium Leves Stop") then
 			if not Player:IsMoving() then
 				stopRunning()
 				backtosleeprandompoint = nil
 				d("stop running")
 			end
 		--if not on map tp to it
-		elseif not iamonmap("Crystarium") then
+		elseif not iAmOnMap("Crystarium") then
 			teleportTo("Crystarium")
 		--if on map but not objective...
-		elseif iamonmap("Crystarium") then
+		elseif iAmOnMap("Crystarium") then
 			--if around aetheryte interact and aetheryte to hawkers' Alley
-			if math.distance3d(Player.pos,{x=-65.02,y=4.53,z=0.02})<=15 then
+			if iAmOnLocRange("Crystarium Aetheryte") then
 				cityBigAetheryteTravel(-65.02,4.53,0.02,9,133,"The Crystalline Mean.")
 			--if somewhere midway between aetheryte and objective 		
-			elseif math.distance3d(Player.pos,{x=-61.98,y=20.00,z=-138.94})<=45 then
+			elseif iAmOnLocRange("Crystarium Leves Area") then
 				--random move to max radius < range where it ends (same pos as where it ends)
 				randomMoveTo(-72.35,20.00,-111.34,0,3)				
 			else
